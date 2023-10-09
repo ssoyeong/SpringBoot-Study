43b152581192aa89f88446b6db6c671049193191
ssoyeong <jsy171771@gmail.com>
2023-10-09 13:19:11 +0900

    feat: add simple GET API

---
+++ src/main/java/com/ssoyeong/studyapplication/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.ssoyeong.studyapplication.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test/hello")
+    public String getHello() {
+        return "Hello world!";
+    }
+}
