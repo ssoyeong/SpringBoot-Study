c6421fe74d1fb85d5a8e6bd7d53215aa7d1fe201
ssoyeong <jsy171771@gmail.com>
2023-10-14 17:08:16 +0900

    feat: add @RequestParam GET API

--- src/main/java/com/ssoyeong/studyapplication/controller/TestController.java
+++ src/main/java/com/ssoyeong/studyapplication/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.ssoyeong.studyapplication.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class TestController {
     @GetMapping("/test/hello")
     public String getHello() {
         return "Hello world!";
+    }
+
+    @GetMapping("/test/greeting")
+    public String getGreeting(@RequestParam String name) {
+        return "Hello, " + name + "!";
     }
 }
