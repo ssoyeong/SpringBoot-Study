4fb5cc80b8e3db62c0ef01283be4e1df691261c1
ssoyeong <jsy171771@gmail.com>
2023-10-17 20:26:10 +0900

    feat: add @PathVariable GET API

--- src/main/java/com/ssoyeong/studyapplication/controller/TestController.java
+++ src/main/java/com/ssoyeong/studyapplication/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.ssoyeong.studyapplication.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,12 @@ public class TestController {
     }
 
     @GetMapping("/test/greeting")
-    public String getGreeting(@RequestParam String name) {
+    public String getGreetingQueryString(@RequestParam String name) {
+        return String.format("Hello, %s!", name);
+    }
+
+    @GetMapping("/test/greeting/{name}")
+    public String getGreetingPathVariable(@PathVariable String name) {
         return String.format("Hello, %s!", name);
     }
 }
