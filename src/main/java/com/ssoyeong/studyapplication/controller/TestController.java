241f90d37155790155d7622c9f7e68a0f3dc7854
ssoyeong <jsy171771@gmail.com>
2023-10-23 21:49:16 +0900

    feat: add @RequestBody POST API

--- src/main/java/com/ssoyeong/studyapplication/controller/TestController.java
+++ src/main/java/com/ssoyeong/studyapplication/controller/TestController.java
@@ -1,26 +1,32 @@
 package com.ssoyeong.studyapplication.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.ssoyeong.studyapplication.dto.UserDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/test")
 public class TestController {
     private static final String GREETING_MESSAGE = "Hello, %s!";
 
-    @GetMapping("/test/hello")
+    @GetMapping("/hello")
     public String getHello() {
         return "Hello world!";
     }
 
-    @GetMapping("/test/greeting")
+    @GetMapping("/greeting")
     public String getGreetingQueryString(@RequestParam String name) {
         return GREETING_MESSAGE.formatted(name);
     }
 
-    @GetMapping("/test/greeting/{name}")
+    @GetMapping("/greeting/{name}")
     public String getGreetingPathVariable(@PathVariable String name) {
         return GREETING_MESSAGE.formatted(name);
+    }
+
+    @PostMapping("/greeting")
+    public ResponseEntity<UserDto> postGreetingRequestBody(@RequestBody UserDto requestUserDto) {
+        UserDto responseUserDto = new UserDto(requestUserDto.name(), requestUserDto.age());
+        return ResponseEntity.ok(responseUserDto);
     }
 }
